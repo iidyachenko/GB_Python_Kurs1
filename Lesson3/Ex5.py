618615f329fb6131561604d10ab0469feed7dafa
Igor_Dyachenko <rick_id@mail.ru>
2020-03-25 21:28:16 +0300

    Выполнено задание 5

---
+++ Lesson3/Ex5.py
@@ -0,0 +1,19 @@
+# Вообще по моему использования глобальных переменных это плохо, но они были в уроке решил попробовать
+
+
+def sum_number(numbers):
+    global num_sum
+    num_list = numbers.split()
+    for num in num_list:
+        if num == 'Q':
+            break
+        num_sum += int(num)
+
+
+num_sum = 0
+while True:
+    user_numbers = input("Введите числа через пробел(для окончания программы нажмите Q): ").upper()
+    sum_number(user_numbers)
+    print("Общая сумма всех введеных чисел: ", num_sum)
+    if user_numbers.find('Q') > 0:
+        break
