a052e26f152850d1e0c837aa325b467844b15373
Igor <rick_id@mail.ru>
2020-03-29 22:56:44 +0300

    Выполнил задание 7

--- Lesson4/Ex2.py
+++ Lesson4/Ex2.py
@@ -1,3 +1,6 @@
+# Представлен список чисел. Необходимо вывести элементы исходного списка, значения которых больше предыдущего элемента.
+
+
 numbers = input("Введите числа через пробел: ").split()
 print(numbers)
 result_numbers = [int(numbers[i]) for i in range(0, len(numbers)) if int(numbers[i]) > int(numbers[i-1])]
