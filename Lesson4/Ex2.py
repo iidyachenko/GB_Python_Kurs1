dc65b77d64566b369c2d0d16d13b280564b786bb
Igor <rick_id@mail.ru>
2020-03-31 18:41:51 +0300

    Исправил задание 2

--- Lesson4/Ex2.py
+++ Lesson4/Ex2.py
@@ -3,5 +3,5 @@
 
 numbers = input("Введите числа через пробел: ").split()
 print(numbers)
-result_numbers = [int(numbers[i]) for i in range(0, len(numbers)) if int(numbers[i]) > int(numbers[i-1])]
+result_numbers = [int(numbers[i]) for i in range(1, len(numbers)) if int(numbers[i]) > int(numbers[i-1])]
 print(result_numbers)
