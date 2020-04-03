cea8af7c8a1c0f2b4f4e525889032178d7c74a12
Igor <rick_id@mail.ru>
2020-04-03 18:26:52 +0300

    Выполнил задание 1

---
+++ Lesson6/Ex1.py
@@ -0,0 +1,23 @@
+# Задание 1. Светофор. Сделал без усложнения с проверкой.
+
+from termcolor import colored
+from time import sleep
+from itertools import cycle
+
+
+class TrafficLight():
+    def __init__(self):
+        self.__color = 'Красный'
+
+    def running(self):
+        colors = [['red', 7, 'Красный'], ['yellow', 2, 'Желтый'], ['green', 7, 'Зеленый']]
+        color = cycle(colors)
+        while True:
+            cur_color = next(color)
+            self.__color = cur_color[2]
+            print(colored('\r' + self.__color, cur_color[0]), end='')
+            sleep(cur_color[1])
+
+
+traffic_light = TrafficLight()
+traffic_light.running()
