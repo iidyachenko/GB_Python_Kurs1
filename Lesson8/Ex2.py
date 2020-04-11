916e9e005f088add3335352abd9e873c65268c86
Igor <rick_id@mail.ru>
2020-04-11 18:54:15 +0300

    Выполнил задание 2

---
+++ Lesson8/Ex2.py
@@ -0,0 +1,17 @@
+class OwnError(Exception):
+    def __init__(self, txt):
+        self.txt = txt
+
+
+inp_data = input("Введите отличное от нуля число: ")
+
+try:
+    inp_data = int(inp_data)
+    if inp_data == 0:
+        raise OwnError("Вы ввели ноль!")
+except ValueError:
+    print("Вы ввели не число")
+except OwnError as err:
+    print(err)
+else:
+    print(f"Все хорошо. Ваше число: {inp_data}")
